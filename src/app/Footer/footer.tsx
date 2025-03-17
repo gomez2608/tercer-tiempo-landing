fe0719b3f93f7f0aea24178d83d8813341280444
Sebastian Gomez Ahumada <s.gomeza1@uniandes.edu.co>
2025-03-16 19:12:36 -0500

    fixed navbar and added hero section

--- src/app/Footer/footer.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Footer() {
-    const currentYear = new Date().getFullYear();
-    return (
-        <footer className="w-full p-4 text-center bg-white shadow-md mt-10">
-            <p>© {currentYear} Tercer tiempo. All rights reserved.</p>
-        </footer>
-    );
-}
